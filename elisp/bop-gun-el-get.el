9aeb402a5a0bcceaa3310f30d06d822398f8fc20
Alex Redington <alex.redington@thinkrelevance.com>
2011-02-07 18:01:35 -0500

    Adding configs.

---
+++ elisp/bop-gun-el-get.el
@@ -0,0 +1,9 @@
+(setq el-get-sources
+      '(;;ack
+	;;(:name smex
+	;;       :type git
+	;;       :url "https://github.com/nonsequitur/smex.git"
+	;;      :after (smex-initialize))
+	))
+
+(el-get)
---
+++ init.el
@@ -0,0 +1,10 @@
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get/")
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+(require 'el-get)
+;;(load "integrations")
+(load "bop-gun-el-get")
+;;(load "custom")
