a3f88d8436924611b1f263b7a57ef998f1d57e74
Alex Redington <alex.redington@thinkrelevance.com>
2011-02-07 18:12:22 -0500

    gitignoring, patching up genned files.

--- elisp/bop-gun-el-get.el
+++ elisp/bop-gun-el-get.el
@@ -1,9 +1,9 @@
 (setq el-get-sources
-      '(;;ack
-	;;(:name smex
-	;;       :type git
-	;;       :url "https://github.com/nonsequitur/smex.git"
-	;;      :after (smex-initialize))
+      '(ack
+	(:name smex
+	       :type git
+	       :url "https://github.com/nonsequitur/smex.git"
+	       :after (smex-initialize))
 	))
 
 (el-get)
