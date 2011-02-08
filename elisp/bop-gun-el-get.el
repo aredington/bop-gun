a1cb39dafc56236672c188462f37b015621768c1
Alex Redington <alex.redington@thinkrelevance.com>
2011-02-07 20:11:34 -0500

    Add clojure mode and paredit mode.

--- elisp/bop-gun-el-get.el
+++ elisp/bop-gun-el-get.el
@@ -1,10 +1,12 @@
 (setq el-get-sources
-      '(ack ruby-mode el-expectations
+      '(ack ruby-mode el-expectations clojure-mode
 	(:name smex
 	       :type git
 	       :url "https://github.com/nonsequitur/smex.git")
 	(:name rspec-mode
 	       :type git
 	       :url "https://github.com/pezra/rspec-mode.git")
+	(:name paredit
+	       :after (lambda () (add-hook 'clojure-mode-hook 'lisp-enable-paredit-hook)))
 	))
 (el-get 'sync)
--- init.el
+++ init.el
@@ -5,6 +5,6 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get/")
 (add-to-list 'load-path "~/.emacs.d/elisp/")
 (require 'el-get)
-;;(load "integrations")
+(load "integrations")
 (load "bop-gun-el-get")
 (load "custom")
