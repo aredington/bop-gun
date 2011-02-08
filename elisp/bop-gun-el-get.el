dd0fec960baba27e0236608997bcfe2aa3f2842a
Alex Redington <alex.redington@thinkrelevance.com>
2011-02-07 19:51:47 -0500

    Add rspec mode.

--- elisp/bop-gun-el-get.el
+++ elisp/bop-gun-el-get.el
@@ -1,9 +1,10 @@
 (setq el-get-sources
-      '(ack ruby-mode
+      '(ack ruby-mode el-expectations
 	(:name smex
 	       :type git
-	       :url "https://github.com/nonsequitur/smex.git"
-	       :after (smex-initialize))
+	       :url "https://github.com/nonsequitur/smex.git")
+	(:name rspec-mode
+	       :type git
+	       :url "https://github.com/pezra/rspec-mode.git")
 	))
-
 (el-get 'sync)
