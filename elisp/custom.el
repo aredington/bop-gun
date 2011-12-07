2a9d5d629ae7ad739276c8edc6ed6284baeaca52
Alex Redington <alex.redington@thinkrelevance.com>
2011-12-06 20:04:00 -0500

    Update readme, switch from cake to lein since cake's pretty dead.

--- elisp/custom.el
+++ elisp/custom.el
@@ -1,8 +1,8 @@
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 210 t)
  '(aquamacs-tool-bar-user-customization nil t)
@@ -14,7 +14,7 @@
  '(global-linum-mode t)
  '(ido-mode (quote both) nil (ido))
  '(indent-tabs-mode nil)
- '(inferior-lisp-program "cake repl")
+ '(inferior-lisp-program "lein repl")
  '(initial-buffer-choice t)
  '(javascript-indent-level 2)
  '(js-indent-level 2)
@@ -33,8 +33,8 @@
 
 ;; Select the color theme
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  )
