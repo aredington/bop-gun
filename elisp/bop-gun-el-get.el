025e6ff264b62bf5975255a3620859ee020117aa
Alex Redington <alex.redington@thinkrelevance.com>
2016-02-02 19:10:22 -0500

    Empty commit message

--- elisp/bop-gun-el-get.el
+++ elisp/bop-gun-el-get.el
@@ -1,7 +1,7 @@
-(setq bopgun-packages '(ack autopair auto-complete cider cm-mode
+(setq bopgun-packages '(ack autopair auto-complete cm-mode
                             clojure-mode coffee-mode color-theme color-theme-ir-black dash
                             el-expectations flymake-ruby haml-mode highlight-symbol js2-mode
                             kibit-mode magit markdown-mode paredit pasm pir-mode rspec-mode
-                            ruby-mode rvm smex sass-mode textmate yaml-mode))
+                            ruby-electric rvm smex sass-mode textmate yaml-mode))
 
 (el-get 'sync bopgun-packages)
