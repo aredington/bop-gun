943f7bac980c209552b58e3c80c2aea101395e64
Alex Redington <alex.redington@thinkrelevance.com>
2016-02-02 19:21:34 -0500

    More fixing updating

--- elisp/bop-gun-el-get.el
+++ elisp/bop-gun-el-get.el
@@ -1,4 +1,4 @@
-(setq bopgun-packages '(ack autopair auto-complete cm-mode
+(setq bopgun-packages '(ack autopair auto-complete rust-mode
                             clojure-mode coffee-mode color-theme color-theme-ir-black dash
                             el-expectations flymake-ruby haml-mode highlight-symbol js2-mode
                             kibit-mode magit markdown-mode paredit pasm pir-mode rspec-mode
