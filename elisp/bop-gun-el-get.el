aec2fab277a5a156c3c6fb613d1258495b4dc11c
Alex Redington <alex.redington@alex.redington>
2020-08-21 11:47:45 -0400

    Update el-get packages so they work

--- elisp/bop-gun-el-get.el
+++ elisp/bop-gun-el-get.el
@@ -1,7 +1,7 @@
-(setq bopgun-packages '(ack autopair auto-complete rust-mode
-                            cider spinner queue clojure-mode coffee-mode color-theme color-theme-ir-black
-                            dash  el-expectations flymake-ruby haml-mode highlight-symbol js2-mode
-                            kibit-mode magit markdown-mode paredit pasm pir-mode rspec-mode
-                            rvm smex sass-mode textmate yaml-mode exec-path-from-shell))
+(setq bopgun-packages '(ack-and-a-half autopair auto-complete rust-mode
+                                       cider spinner queue clojure-mode coffee-mode color-theme color-theme-ir-black
+                                       dash  el-expectations flymake-ruby haml-mode highlight-symbol js2-mode
+                                       kibit-mode magit markdown-mode paredit parseedn pasm pir-mode rspec-mode
+                                       rvm smex sass-mode sesman textmate yaml-mode exec-path-from-shell))
 
 (el-get 'sync bopgun-packages)
